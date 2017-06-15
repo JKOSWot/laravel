8fb5be48e47a4cc1d0674053c56a42a3ad824530
Jake <jake_kingsley@live.co.uk>
2017-06-15 16:16:25 +0100

    Create home.blade.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1>My Portfolio</h1>      
+    <p>Some text that represents "Me"...</p>
+  </div>
+</div>
+@endsection
