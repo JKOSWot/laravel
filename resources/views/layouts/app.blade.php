2c427f296f2e705194fc4f2ec2134a130eb2307d
Jake <jake_kingsley@live.co.uk>
2017-06-15 16:09:18 +0100

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('layouts.partials.header')
+
+@include('layouts.partials.nav')
+  
+@yield('content')
+
+@include('layouts.partials.footer')
+
+</body>
+</html>
